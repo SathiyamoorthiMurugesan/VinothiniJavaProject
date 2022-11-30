19232fb1298d6eb43b649dfe2cc054bfd786e764
SathiyamoorthiMurugesan <sathya08ece@gmail.com>
2022-11-30 09:11:00 +0530

    testing

--- src/test/java/seleniumBasics/CovertingToMethods.java
+++ src/test/java/seleniumBasics/CovertingToMethods.java
@@ -1,10 +1,5 @@
 package seleniumBasics;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,6 +14,7 @@ public class CovertingToMethods {
 	public static void main(String[] args) throws Exception {
 		CovertingToMethods obj = new CovertingToMethods();
 		obj.facebookLogin();
+		obj.OpenBrowserWithParameter("Chrome");
 	}
 	
 	public void facebookLogin() throws Exception {
@@ -41,6 +37,24 @@ public class CovertingToMethods {
 	public void test1() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\git\\VinothiniJavaProject\\drivers\\chromedriver_107.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("");
+		driver.findElement(By.id("email")).sendKeys("Sathya08ece@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("TestSathya@123");
+		driver.findElement(By.name("login")).click();
+	}
+	
+	public void OpenBrowserWithParameter(String browserName) {
+		switch(browserName) {
+			case "Chrome":
+				System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\git\\VinothiniJavaProject\\drivers\\chromedriver_107.exe");
+				driver = new ChromeDriver();
+				break;
+			case "Firefox":
+				System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\git\\VinothiniJavaProject\\drivers\\geckodriver.exe");
+				driver = new ChromeDriver();
+				break;
+		}
 		driver.manage().window().maximize();
 		driver.get("");
 		driver.findElement(By.id("email")).sendKeys("Sathya08ece@gmail.com");
